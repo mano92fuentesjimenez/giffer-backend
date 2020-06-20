1d578f3bde3406b7508bd61e5c47a5dba30b1a85
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-06-20 15:53:08 -0400

    add mock giphy response

--- helpers/giphyRequest.js
+++ helpers/giphyRequest.js
@@ -3,10 +3,13 @@ const request = require('superagent');
 
 const apikey = config.get('giphy.apiKey');
 const giphyBasePath = config.get('giphy.basePath');
+const mockResponse = require('./giffyResponseMock.json');
 
 module.exports = async (path, query) => {
-  const d = await request.get(`${giphyBasePath}/gifs/trending`)
-    .query({ ...query, api_key: apikey });
+  // const d = await request.get(`${giphyBasePath}/gifs/trending`)
+  //   .query({ ...query, api_key: apikey });
+  //
+  // return d.body;
 
-  return d.body;
+  return Promise.resolve(mockResponse);
 }
